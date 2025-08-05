895eb1191fcf7e637dd55a831103c5e710b0d804
Lee310-tree <220035745+Lee310-tree@users.noreply.github.com>
2025-08-05 20:19:30 +0900

    [Bronze V] Title: 사분면 고르기, Time: 168 ms, Memory: 17716 KB -BaekjoonHub

--- 백준/Bronze/14681. 사분면 고르기/사분면 고르기.java
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.java
@@ -2,20 +2,20 @@ import java.util.Scanner;
 public class Main{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        
-        int X = sc.nextInt();
-        int Y = sc.nextInt();
+        int x = sc.nextInt();
+        int y = sc.nextInt();
         
         sc.close();
         
-        if(X>0){
-            if(Y>0){ System.out.println("1");}
-            else{System.out.println("4");}
+        if(x>0){
+            if(y>0) System.out.println("1");
+            else if(y<0) System.out.println("4");
+        }
+        else {
+            if(y>0) System.out.println("2");
+            else if(y<0) System.out.println("3");
             
         }
-        else{if(Y>0){System.out.println("2");}
-            
-            else{System.out.println("3");}
-            }
+        
     }
 }
