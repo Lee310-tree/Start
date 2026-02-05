801734b41f60284bb7acc52df847c8b0250a3a0a
Lee310-tree <220035745+Lee310-tree@users.noreply.github.com>
2026-02-05 21:19:32 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.py
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.py
@@ -1,8 +1,4 @@
 S = input()
-abc ='abcdefghijklmnopqrstuvwxyz'
-
-for i in abc:
-    if i in S:
-        print(S.index(i), end= ' ')
-    else:
-        print( -1, end =' ')
+a = 'abcdefghijklmnopqrstuvwxyz'
+for i in a:
+    print(S.find(i), end =' ')
